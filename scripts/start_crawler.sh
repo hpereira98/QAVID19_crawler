8ada2af78531dfd8b73664fb102a11eea1165692
hpereira98 <pereirapalmeirahenrique@gmail.com>
2021-04-28 23:32:33 +0100

    added configs for covid_pt/en + url_regex model

--- scripts/start_crawler.sh
+++ scripts/start_crawler.sh
@@ -5,13 +5,14 @@
 # version: 1.0.0       #   
 ########################
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "You need to specify the crawler application."
-    echo "Example: ./start_crawler.sh <your_application>"
+    echo "Example: ./start_crawler.sh <your_application> <classification_model>"
     exit 1
 fi
 
 CRAWLER_APPLICATION=$1
+CLASSIFICATION_MODEL=$2
 
 ENV_FILE=../configs/${CRAWLER_APPLICATION}/settings.env
 
@@ -30,13 +31,14 @@ echo "Loading variables from: ${ENV_FILE} ..."
 CONFIGS_FOLDER=${ROOT_FOLDER}/configs/${CRAWLER_APPLICATION}
 DATA_FOLDER=${ROOT_FOLDER}/data/${CRAWLER_APPLICATION}
 DOCKER_CONTAINER_NAME=${CRAWLER_APPLICATION}_crawler
-
+MODELS_FOLDER=${ROOT_FOLDER}/models/${CRAWLER_APPLICATION}
 
 # log folders
 echo ""
 echo "CONFIGS_FOLDER: ${CONFIGS_FOLDER}"
 echo "DATA_FOLDER: ${DATA_FOLDER}"
 echo "DOCKER_CONTAINER_NAME: ${DOCKER_CONTAINER_NAME}"
+echo "MODELS_FOLDER: ${MODELS_FOLDER}"
 
 # start crawler
 echo ""
@@ -50,18 +52,20 @@ echo "Starting the Crawler..."
 nohup sudo docker run \
     -v ${CONFIGS_FOLDER}:/configs/${CRAWLER_APPLICATION} \
     -v ${DATA_FOLDER}:/data/${CRAWLER_APPLICATION} \
+    -v ${MODELS_FOLDER}/${CLASSIFICATION_MODEL}:/models/${CRAWLER_APPLICATION}/${CLASSIFICATION_MODEL} \
     -p 8080:8080 \
     --name ${DOCKER_CONTAINER_NAME} \
     vidanyu/ache startCrawl \
     -c /configs/${CRAWLER_APPLICATION}/ \
     -s /configs/${CRAWLER_APPLICATION}/seeds.txt \
     -o /data/${CRAWLER_APPLICATION}/ \
+    -m /models/${CRAWLER_APPLICATION}/${CLASSIFICATION_MODEL}/ \
     &>${OUTPUT_LOG_FILE} &
 
 # wait 60 seconds before checking if it is running
 echo ""
-echo "COFFEE TIME! The process will now wait 60 seconds before validating if all went well."
-sleep 60
+echo "COFFEE TIME! The process will now wait 30 seconds before validating if all went well."
+sleep 30
 
 # check if it is running
 echo ""
